56c28c05dc4071dd2deba9fb78ef6f604c6ad241
rayanish92 <rayanish92@gmail.com>
2026-05-10 18:59:51 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -4,6 +4,7 @@ const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  contactNumber: { type: String, default: '' }, // NEW: Added Contact Number
   role: {
     type: String,
     enum: ['admin', 'retailer', 'customer', 'delivery_agent'],
@@ -11,13 +12,12 @@ const UserSchema = new mongoose.Schema({
   },
   location: {
     type: { type: String, default: 'Point' },
-    coordinates: { type: [Number], default: [0, 0] }
+    coordinates: { type: [Number], default: [0, 0] } // [longitude, latitude]
   },
   shopName: { type: String, default: '' },
-  retailerCategory: { type: String, default: '' }, // NEW: E.g., Groceries, Pharmacy, Electronics
+  retailerCategory: { type: String, default: '' },
   isAvailable: { type: Boolean, default: true }
 }, { timestamps: true });
 
 UserSchema.index({ location: "2dsphere" });
-
 module.exports = mongoose.model('User', UserSchema);
