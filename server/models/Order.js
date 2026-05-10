87c4682fde649f2270ec4fefada101b8e443288d
rayanish92 <rayanish92@gmail.com>
2026-05-10 19:44:59 +0530

    Update Order.js

--- server/models/Order.js
+++ server/models/Order.js
@@ -2,9 +2,13 @@ const mongoose = require('mongoose');
 
 const OrderSchema = new mongoose.Schema({
   orderId: { type: String, required: true, unique: true },
+  customerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   retailerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  items: { type: Array, required: true }, // Stores the cart
   totalAmount: { type: Number, required: true },
-  status: { type: String, default: 'Completed' }
+  paymentMethod: { type: String, required: true },
+  deliveryAddress: { type: String, required: true },
+  status: { type: String, default: 'Order Placed' }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Order', OrderSchema);
