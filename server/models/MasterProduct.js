3fcef9556de2039c5e6e5dd9729e0815f3096b9d
rayanish92 <rayanish92@gmail.com>
2026-05-10 15:23:41 +0530

    Update MasterProduct.js

--- server/models/MasterProduct.js
+++ server/models/MasterProduct.js
@@ -4,7 +4,7 @@ const MasterProductSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String },
   category: { type: String, required: true },
-  imageUrl: { type: String, required: true }, // Admin must provide a photo
+  imageUrl: { type: String, default: '' }, // FIX: Removed 'required: true' to make it optional
 }, { timestamps: true });
 
 module.exports = mongoose.model('MasterProduct', MasterProductSchema);
