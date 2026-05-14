6e6a98d725572b31574ffec6e5560b59e2650e03
rayanish92 <rayanish92@gmail.com>
2026-05-14 10:31:54 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,20 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-
-// ONLY IMPORT THIS EXACTLY ONCE
 import { registerSW } from 'virtual:pwa-register';
 
-const updateSW = registerSW({
-  onNeedRefresh() {
-    if (confirm('New system update available! Click OK to refresh.')) {
-      updateSW(true);
-    }
-  },
-  onOfflineReady() {
-    console.log('App is cached and ready to work offline.');
-  },
-});
+// Register the Service Worker immediately
+registerSW({ immediate: true });
 
 const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
@@ -25,5 +15,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
       <App />
     </GoogleOAuthProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
